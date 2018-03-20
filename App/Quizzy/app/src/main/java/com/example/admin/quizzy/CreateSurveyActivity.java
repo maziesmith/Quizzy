dc1bbae26dca7fb12195cd2f901c52060358de71
mbertucci1 <michael_bertucci@student.uml.edu>
2018-03-19 23:16:06 -0400

    Initial android studio project files

---
+++ App/Quizzy/app/src/main/java/com/example/admin/quizzy/CreateSurveyActivity.java
@@ -0,0 +1,17 @@
+package com.example.admin.quizzy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 3/19/2018.
+ */
+
+public class CreateSurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_survey);
+    }
+}
