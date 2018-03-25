ab256830309bc75f8871542e38617ea359cea5a4
mbertucci1 <michael_bertucci@student.uml.edu>
2018-03-25 13:37:33 -0400

    More work on CreateSurveyActivity
    Need to customize listview to have different layouts for different numbers of responses

--- App/Quizzy/app/src/main/java/com/example/admin/quizzy/SurveyItem.java
+++ App/Quizzy/app/src/main/java/com/example/admin/quizzy/SurveyItem.java
@@ -13,20 +13,43 @@ public class SurveyItem {
     String _question;
     int _numResponses;
     ArrayList<String> _responses;
+    static final int MAX_RESPONSES = 6;
 
     public SurveyItem(int numResponses) {
-        _numResponses = numResponses;
+        if(numResponses < 1) {
+            _numResponses = 1;
+        } else if(numResponses > MAX_RESPONSES) {
+            _numResponses = MAX_RESPONSES;
+        } else {
+            _numResponses = numResponses;
+        }
         _question = "";
-        _responses = new ArrayList<>(numResponses);
+        _responses = new ArrayList<String>(numResponses);
         for(int i = 0; i < numResponses; i++) {
             _responses.add("");
         }
     }
 
-    public SurveyItem(String question, ArrayList<String> responses) {
+    public SurveyItem(String question, String[] responses) {
+        _responses = new ArrayList<String>();
+
+        if(responses.length < 1) {
+            _responses.add("");
+        } else {
+            for (int i = 0; i < responses.length && i < MAX_RESPONSES; i++) {
+                _responses.add(responses[i]);
+            }
+        }
         _question = question;
-        _responses = responses;
         _numResponses = _responses.size();
+    }
+
+    public String getQuestion() {
+        return _question;
+    }
+
+    public ArrayList<String> getResponses() {
+        return _responses;
     }
 
     public void setQuestion(String question) {
