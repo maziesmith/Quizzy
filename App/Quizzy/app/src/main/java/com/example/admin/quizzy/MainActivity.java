0f8fe1a3a696ef16a49682556e51b5d8028436d6
Brian Medina <probune@gmail.com>
2018-04-20 21:32:52 -0400

    logout almost works
    but doesn't yet

--- App/Quizzy/app/src/main/java/com/example/admin/quizzy/MainActivity.java
+++ App/Quizzy/app/src/main/java/com/example/admin/quizzy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin.quizzy;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.squareup.moshi.JsonAdapter;
@@ -14,10 +18,13 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private final OkHttpClient client = new OkHttpClient();
     private final Moshi moshi = new Moshi.Builder().build();
 
+    // bind views
+    @BindView(R.id.logoutButton) Button _logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
 
         // set adapter for listview
         _listView.setAdapter(adapter);
+
+        // set logout onclick
+        _logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+
+
     }
 
     public MainActivity() {
@@ -89,4 +108,69 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void logOut(){
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+
+        // TODO: Fix request body
+        RequestBody body = RequestBody.create(mediaType, "{\n\t\"username\" : \"brian\",\n\t\"password\" :\"brian\"\n}");
+
+        
+        Request request = new Request.Builder()
+                .url("http://quizzybackend.herokuapp.com/user/logout")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Postman-Token", "4126f96d-34dd-4ab5-8729-609bedbc22d0")
+                .build();
+
+        // makes an asynchronous call for network io
+        client.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(final Call call, IOException e) {
+                        // Error
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // main thread stuff here
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        try {
+                            String res = response.body().string();
+
+                            // use moshi to turn it into an object for easy access
+                            JsonAdapter<logoutResponse> jsonAdapter = moshi.adapter(logoutResponse.class);
+                            // throws JsonDataException if it doesn't fit in response class
+                            logoutResponse l = jsonAdapter.fromJson(res);
+                            if(l != null && !l.logged_in){
+                                // write email and logged in flag to shared pref
+                                SharedPreferences pref = getApplicationContext().getSharedPreferences("quizzy.pref", 0);
+                                SharedPreferences.Editor editor = pref.edit();
+                                editor.putBoolean("logged_in", false);
+                                editor.apply();
+
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+
+                            }
+                        } catch (JsonDataException ignored) {
+                        } catch (IOException ignored) {
+                        }
+                    }
+                });
+    }
+
+    static class logoutResponse{
+        public int id;
+        public String username;
+        public Boolean logged_in;
+    }
+
 }
